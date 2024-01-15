c428d2d907227b2779176c1265dfd8760f6d46f2
Student <Student@PC-1951>
2024-01-15 12:24:33 +0800

    Add project files.

---
+++ Numerical/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleApp7
+{
+    public class Calc
+    {
+        static void Main(string[] args)
+        {
+            int x = 0, y = 0;
+            Console.Write("Enter the first number: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the second number: ");
+            y = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+        }
+        public void Addition(int x, int y)
+        {
+            Console.WriteLine("Addition of both numbers is {0}", (x + y));
+        }
+        public void Subtraction(int x, int y)
+        {
+            Console.WriteLine("Subtraction of both numbers is {0}", (x - y));
+        }
+        public void Multiplication(int x, int y)
+        {
+            Console.WriteLine("Multiplication of both number is {0}", (x * y));
+        }
+    }
+}
