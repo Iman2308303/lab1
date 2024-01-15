ca40ad58e71a3765aefbe91b44c90299329f19d5
Shah Adam Ludwig Shah Jahankhir Bin Abdul 'Adl Shah Adam Ludwig Shah Jahankhir Bin Abdul 'Adl <2308313@student.toa.edu.my>
2024-01-15 12:34:22 +0800

    adam part

--- Numerical/Program.cs
+++ Numerical/Program.cs
@@ -30,7 +30,7 @@ namespace ConsoleApp7
             Console.WriteLine("Multiplication of both number is {0}", (x * y));
                         Console.WriteLine("Multiplication of both number is {0}", (x * y));
             Console.WriteLine("Multiplication of both number is {0}", (x * y));
-
+            ConsoleConsole.WriteLine("this adam code");
 
         }
     }
