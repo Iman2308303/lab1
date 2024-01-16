5fa2748664b44a66f71d7da9e30db6107558a93f
Amirul Iman Bin Rusly Amirul Iman Bin Rusly <2308303@student.toa.edu.my>
2024-01-16 22:04:38 +0800

    Description of changes made for the task using new branch

--- Numerical/Program.cs
+++ Numerical/Program.cs
@@ -3,46 +3,55 @@ using System.Security.Cryptography.X509Certificates;
 
 namespace ConsoleApp7
 {
-    public class Calc
+    namespace ConsoleApp7
     {
-        static void Main(string[] args)
+        public class Calculator
         {
-            char z;
-            do
+            static void Main(string[] args)
             {
-                int x = 0, y = 0;
-                Console.Write("Enter the first number: ");
-                x = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Enter the second number: ");
-                y = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("\n");
+                char z;
+                do
+                {
+                    int x = 0, y = 0;
+                    Console.Write("Enter the first number: ");
+                    x = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Enter the second number: ");
+                    y = Convert.ToInt32(Console.ReadLine());
 
-                Addition(x, y);
-                Subtraction(x, y);
-                Multiplication(x, y);
-                Division(x, y);
+                    Console.Write("Choose an operation (+, -, *, /): ");
+                    char operation = Convert.ToChar(Console.ReadLine());
 
-                Console.WriteLine("\n");
-                Console.WriteLine("Do you want to enter new values? (y/n)");
-                z = Convert.ToChar(Console.ReadLine());
-            } while (z == 'y' || z == 'Y');
-        }
-        static void Addition(int x, int y)
-        {
-            Console.WriteLine("Addition of both numbers is {0}", (x + y));
-        }
-        static void Subtraction(int x, int y)
-        {
-            Console.WriteLine("Subtraction of both numbers is {0}", (x - y));
-        }
-        static void Multiplication(int x, int y)
-        {
-            Console.WriteLine("Multiplication of both number is {0}", (x * y));
-            
-        }
-        static void Division(int x, int y)
-        {
-            Console.WriteLine("Division of both numbers is {0}", (x / y));
+                    PerformOperation(x, y, operation);
+
+                    Console.WriteLine("\nDo you want to perform another calculation? (y/n)");
+                    z = Convert.ToChar(Console.ReadLine());
+                } while (z == 'y' || z == 'Y');
+            }
+
+            static void PerformOperation(int x, int y, char operation)
+            {
+                switch (operation)
+                {
+                    case '+':
+                        Console.WriteLine($"Addition of {x} and {y} is {x + y}");
+                        break;
+                    case '-':
+                        Console.WriteLine($"Subtraction of {x} and {y} is {x - y}");
+                        break;
+                    case '*':
+                        Console.WriteLine($"Multiplication of {x} and {y} is {x * y}");
+                        break;
+                    case '/':
+                        if (y != 0)
+                            Console.WriteLine($"Division of {x} by {y} is {x / (double)y}");
+                        else
+                            Console.WriteLine("Cannot divide by zero!");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid operation. Please choose +, -, *, or /.");
+                        break;
+                }
+            }
         }
     }
 }
